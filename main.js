5aa1281fedb9baad23e623592666c92a13c2bbb1
Jirong <munja__@naver.com>
2021-12-01 01:10:09 +0900

    ADD: 1. 각 control 입력 시 나머지 컨트롤 안보이게 수정. 2. create 시 author combobox 추가.

--- main.js
+++ main.js
@@ -50,22 +50,32 @@ var app = http.createServer(function(request, response) {
       if (error) {
         console.log(error);
       }
-      console.log(`info : ${info}`);
-      var list = OBJ_template.list(info);
-      var template = OBJ_template.HTML(list, '', control);
-      template += `
-      <form action="/create_preocess" method = "post">
-        <p><input type="text" name="title" placeholder="title"></p>
-        <p>
-          <textarea name="description" placeholder="description"></textarea>
-        </p>
-        <p>
-          <input type="submit">
-        </p>
-      </form>
-      `
-      response.writeHead(200);
-      response.end(template);
+      var authorList = "";
+      db.query('SELECT * FROM author', function(error, authors) {
+        for (var i = 0; i < authors.length; i++) {
+          authorList += `<option value="${authors[i].id}">${authors[i].name}</option>`;
+        }
+        var list = OBJ_template.list(info);
+        var template = OBJ_template.HTML(list, '', '');
+        template += `
+        <form action="/create_preocess" method = "post">
+          <p><input type="text" name="title" placeholder="title"></p>
+          <p>
+            <textarea name="description" placeholder="description"></textarea>
+          </p>
+          <p>
+          <select name="author">
+            ${authorList}
+          </select>
+          </p>
+          <p>
+            <input type="submit">
+          </p>
+        </form>
+        `
+        response.writeHead(200);
+        response.end(template);
+      });
     });
   } else if (pathname === '/create_preocess') {
     var body = '';
@@ -75,10 +85,11 @@ var app = http.createServer(function(request, response) {
     });
     request.on('end', function() {
       var post = qs.parse(body);
+      console.log(`post.author = ${post.author}`);
       db.query(`
         INSERT INTO INFO (title, description, created, author_id)
         VALUES(?, ?, NOW(), ?)`,
-        [post.title, post.description, 1],
+        [post.title, post.description, post.author],
         function(error, result) {
           if (error) {
             throw error;
@@ -96,7 +107,7 @@ var app = http.createServer(function(request, response) {
         console.log(error);
       }
       var list = OBJ_template.list(info);
-      var template = OBJ_template.HTML(list, '', control);
+      var template = OBJ_template.HTML(list, '', '');
       db.query(`SELECT * FROM INFO WHERE id = ${queryData.id}`, function(error, result){
         console.log(result);
         template += `
