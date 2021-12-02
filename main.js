95a6f4abe994d1c2fa15090f4650f032800e43e1
Jirong <munja__@naver.com>
2021-12-03 00:14:31 +0900

    FIX:  1. Auto_increasement로 인해 delete 시 home으로 가지 않는 문제 해결.(기존은 id가 1번인 값으로 돌아감)  2. Database의 Table이 비었을 때 예외조건 추가

--- main.js
+++ main.js
@@ -28,21 +28,49 @@ var app = http.createServer(function(request, response) {
     db.query('SELECT * FROM INFO', function(error, info, fields) {
       if (error) {
         console.log(error);
-      }
-      if (filePath === '/') {
-        queryData.id = info[0].id;
-      }
-      db.query('SELECT * FROM INFO LEFT JOIN author ON INFO.author_id = author.id WHERE INFO.id=?',[queryData.id], function(error, result) {
-        var list = OBJ_template.list(info);
-        var description;
-        db.query(`SELECT * FROM INFO WHERE id = ${queryData.id}`, function(error, res_des){
-          description = res_des[0].description;
-        var template = OBJ_template.HTML(list, description, control);
-        template+=`<p>by ${result[0].name}</p>`
-        response.writeHead(200);
-        response.end(template);
+      } else if (!info.length) {
+        var authorList = "";
+        db.query('SELECT * FROM author', function(error, authors) {
+          for (var i = 0; i < authors.length; i++) {
+            authorList += `<option value="${authors[i].id}">${authors[i].name}</option>`;
+          }
+          var template = OBJ_template.HTML('', '', '');
+          template += `
+            <h2>파일이 하나도 없네요! 만들어보세요~</h2>
+            <form action="/create_preocess" method = "post">
+              <p><input type="text" name="title" placeholder="title"></p>
+              <p>
+                <textarea name="description" placeholder="description"></textarea>
+              </p>
+              <p>
+              <select name="author">
+                ${authorList}
+              </select>
+              </p>
+              <p>
+                <input type="submit">
+              </p>
+            </form>
+            `
+          response.writeHead(200);
+          response.end(template);
         });
-      });
+      } else {
+        if (filePath === '/') {
+          queryData.id = info[0].id;
+        }
+        db.query('SELECT * FROM INFO LEFT JOIN author ON INFO.author_id = author.id WHERE INFO.id=?', [queryData.id], function(error, result) {
+          var list = OBJ_template.list(info);
+          var description;
+          db.query(`SELECT * FROM INFO WHERE id = ${queryData.id}`, function(error, res_des) {
+            description = res_des[0].description;
+            var template = OBJ_template.HTML(list, description, control);
+            template += `<p>by ${result[0].name}</p>`
+            response.writeHead(200);
+            response.end(template);
+          });
+        });
+      }
     });
   } else if (pathname === '/create') {
     db.query('SELECT * FROM INFO', function(error, info, fields) {
@@ -105,18 +133,18 @@ var app = http.createServer(function(request, response) {
       if (error) {
         console.log(error);
       }
-        var list = OBJ_template.list(info);
-        var template = OBJ_template.HTML(list, '', '');
-        db.query(`SELECT * FROM INFO WHERE id = ${queryData.id}`, function(error, result){
-          var authorList = "";
-          db.query('SELECT * FROM author', function(error, authors) {
-            for (var i = 0; i < authors.length; i++) {
-              var selected = '';
-              if(result[0].author_id == authors[i].id){
-                selected = ' selected';
-              }
-              authorList += `<option value="${authors[i].id}"${selected}>${authors[i].name}</option>`;
+      var list = OBJ_template.list(info);
+      var template = OBJ_template.HTML(list, '', '');
+      db.query(`SELECT * FROM INFO WHERE id = ${queryData.id}`, function(error, result) {
+        var authorList = "";
+        db.query('SELECT * FROM author', function(error, authors) {
+          for (var i = 0; i < authors.length; i++) {
+            var selected = '';
+            if (result[0].author_id == authors[i].id) {
+              selected = ' selected';
             }
+            authorList += `<option value="${authors[i].id}"${selected}>${authors[i].name}</option>`;
+          }
           console.log(result);
           template += `
               <form action="/update_preocess" method = "post">
@@ -151,14 +179,16 @@ var app = http.createServer(function(request, response) {
       console.log(post);
 
       db.query('UPDATE INFO SET title=?, description=?, author_id=? WHERE id=?',
-      [post.title, post.description, post.author, post.id],
-      function(error, result){
-        if(error){
-          throw error;
-        }
-        response.writeHead(302, { Location: `/?id=${post.id}`});
-        response.end();
-      });
+        [post.title, post.description, post.author, post.id],
+        function(error, result) {
+          if (error) {
+            throw error;
+          }
+          response.writeHead(302, {
+            Location: `/?id=${post.id}`
+          });
+          response.end();
+        });
     });
   } else if (pathname === '/delete_process') {
     var body = '';
@@ -168,15 +198,25 @@ var app = http.createServer(function(request, response) {
     });
     request.on('end', function() {
       var post = qs.parse(body);
-      db.query(`DELETE FROM INFO WHERE id = ${post.id}`, function(error, result){
-        response.writeHead(302, {Location: `/?id=1`});
+      db.query(`DELETE FROM INFO WHERE id = ${post.id}`, function(error, result) {
+        db.query('SELECT * FROM INFO', function(error, info) {
+          if(!info.length){
+            response.writeHead(302, {
+            Location: `/`
+          });
+          }
+          else {
+            response.writeHead(302, {
+            Location: `/?id=${info[0].id}`
+          });
+        }
         response.end();
+        });
       })
     });
   } else {
     response.writeHead(404);
     response.end('Not found');
   }
-
 });
 app.listen(3000);
