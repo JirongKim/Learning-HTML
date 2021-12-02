99989e8da666bf38a6caa0ba3999e697b6458560
Jirong <munja__@naver.com>
2021-12-03 00:37:31 +0900

    CLN:  1. info.js 추가 후 home 모듈 생성

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ var path = require('path');
 var OBJ_template = require('./lib/TEMPLATE.js');
 var mysql = require('mysql');
 var db = require('./lib/db.js');
+var INFO = require('./lib/info.js');
 
 var app = http.createServer(function(request, response) {
   var _url = request.url;
@@ -22,32 +23,7 @@ var app = http.createServer(function(request, response) {
       if (error) {
         console.log(error);
       } else if (!info.length) {
-        var authorList = "";
-        db.query('SELECT * FROM author', function(error, authors) {
-          for (var i = 0; i < authors.length; i++) {
-            authorList += `<option value="${authors[i].id}">${authors[i].name}</option>`;
-          }
-          var template = OBJ_template.HTML('', '', '');
-          template += `
-            <h2>파일이 하나도 없네요! 만들어보세요~</h2>
-            <form action="/create_preocess" method = "post">
-              <p><input type="text" name="title" placeholder="title"></p>
-              <p>
-                <textarea name="description" placeholder="description"></textarea>
-              </p>
-              <p>
-              <select name="author">
-                ${authorList}
-              </select>
-              </p>
-              <p>
-                <input type="submit">
-              </p>
-            </form>
-            `
-          response.writeHead(200);
-          response.end(template);
-        });
+        INFO.home(request, response);
       } else {
         if (filePath === '/') {
           queryData.id = info[0].id;
